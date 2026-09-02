75db3224f0184b9f3e7a86e5321740297a75300b
Mohamed Bahig <mbahig5@gmail.com>
2026-09-02 16:52:13 +0300

    Add files via upload

---
+++ frist.py
@@ -0,0 +1,9 @@
+#----------------------------------------------------
+# Information About File
+# License
+# Who Created The File  
+# when The File Was Created
+# why The File Was Created 
+#----------------------------------------------------
+print("I love python")
+print("I LOVE programming!") # Created by: BahigDev
